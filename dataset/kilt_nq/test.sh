49f2417d959d1b40953299c8318316ab432c9576
amy-hyunji <alee6868@kaist.ac.kr>
2022-05-31 23:08:18 +0900

    update(*): update for merge

--- dataset/kilt_nq/test.sh
+++ dataset/kilt_nq/test.sh
@@ -1,2 +1,2 @@
 #python extract_embedding.py --corpus nq_toy_corpus.csv --train_file nq_toy_train.csv --dev_file nq_toy_dev.csv --test_file nq_toy_test.csv --save_path test --emb_path t5-base
-python extract_embedding.py --corpus nq_toy_corpus.csv --train_file nq_toy_train.csv --dev_file nq_toy_dev.csv --test_file nq_toy_test.csv --save_path bert-base-cased --emb_path bert-base-cased
+python extract_embedding.py --corpus nq_toy_corpus.csv --train_file nq_toy_train.csv --dev_file nq_toy_dev.csv --test_file nq_toy_test.csv --save_path bert-base-cased-test --emb_path bert-base-cased
